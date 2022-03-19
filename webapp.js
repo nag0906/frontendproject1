f6b4bf5a313df9eafa41ad7cfb496d17ad932133
nag0906 <101916932+nag0906@users.noreply.github.com>
2022-03-19 10:45:18 +0530

    Add files via upload

---
+++ webapp.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#searchform');
+const res = document.querySelector('#result');
+var upd;
+form.addEventListener('submit', (e) => {
+    if(upd){
+        clearTimeout(upd);
+    }
+    e.preventDefault();
+    const ctype = form.elements.cointype.value;
+
+    fetchprice(ctype);
+});
+const fetchprice = async (ctype) => {
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+    // console.log(r);
+    const price = r.data.coin.price;
+    const change = r.data.coin.priceChange1d;
+    
+    const volume = r.data.coin.volume;
+    const base = r.data.coin.name;
+    const target= 'INR';
+    var col ="green";
+    if(change<0){
+     col = "red";
+    }
+    res.innerHTML = ` <tr>
+    <td style="color: white; font-family: 'Rokkitt', serif;background-color: #54c9a6; "> Property </td>
+     <td style="color: white; font-family: 'Rokkitt', serif;background-color: #54c9a6;">value </td>
+   </tr>
+   <tr>
+       <td style="color: white; font-family: 'Rokkitt', serif;">${base}</td>
+       <td style ="color:${col}" > ${price} ${target}</td>
+   </tr><tr>
+           <td style="color: white; font-family: 'Rokkitt', serif;"> change </td>
+           <td style ="color:${col}" >${change}</td>
+       </tr><tr>
+           <td style="color: white; font-family: 'Rokkitt', serif;">volume</td>
+           <td style="color: white; font-family: 'Rokkitt', serif;">${volume}</td>
+       </tr> `
+
+upd = setTimeout(()=> fetchprice(ctype),10000);
+
+}
